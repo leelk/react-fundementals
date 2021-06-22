a30a271ec484519c0267fd31d4e1a0730291dc8b
Leel Karunarathne <leel@rumex.lk>
2021-06-23 00:04:17 +0530

    Add Functional Component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
+        <Home/>
       <Greet/>
         <Welcome/>
     </div>
